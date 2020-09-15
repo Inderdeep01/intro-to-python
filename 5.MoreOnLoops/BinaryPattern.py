805b52da642aa64ea47aa836427c47bd9be306ab
Inderdeep Singh <inderdeepsidhu01@gmail.com>
2020-09-15 19:59:12 +0530

    BinaryPattern.py

---
+++ 5.MoreOnLoops/BinaryPattern.py
@@ -0,0 +1,31 @@
+"""
+Binary Pattern
+Send Feedback
+Print the following pattern for the given number of rows.
+Pattern for N = 4
+1111
+000
+11
+0
+Input format : N (Total no. of rows)
+
+Output format : Pattern in N lines
+
+Sample Input :
+5
+Sample Output :
+11111
+0000
+111
+00
+1
+"""
+
+n=int(input())
+for i in range(1,n+1):
+    for l in range(n+2-2*i,0,-1):
+        print(1,end="")
+    print()
+    for j in range (n+1-2*i,0,-1):
+        print(0,end="")
+    print()
